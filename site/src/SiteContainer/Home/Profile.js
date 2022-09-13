517df3ae847395b7955e3273dd2a397d4687951e
Angeredspy <christo.graham1@gmail.com>
2022-09-13 11:32:33 -0400

    renders initial icons via fontawesome & bootstrap

--- site/src/SiteContainer/Home/Profile.js
+++ site/src/SiteContainer/Home/Profile.js
@@ -2,7 +2,22 @@ import React from 'react';
 
 export default function Profile() {
     return (
-        <div>
+        <div className='profile-container'>
+            <div className='profile-parent'>
+                <div className='profile-details'>
+                    <div className='cols'>
+                        <a href ='#'>
+                            <i className ='fa fa-github-alt'></i>
+                        </a>
+                        <a href ='#'>
+                            <i className ='fa fa-plus-square'></i>
+                        </a>
+                        <a href ='#'>
+                            <i className ='fa fa-globe'></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
 
         </div>
     )
