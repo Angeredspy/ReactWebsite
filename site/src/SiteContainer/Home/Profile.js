9b22a5501d8dcce1bdf6c08e51af72a243de1c75
Angeredspy <christo.graham1@gmail.com>
2022-09-13 14:03:26 -0400

    Adds resume & attributes for new tab

--- site/src/SiteContainer/Home/Profile.js
+++ site/src/SiteContainer/Home/Profile.js
@@ -1,54 +1,79 @@
-import React from 'react'; 
-import Typical from 'react-typical';
+import React from "react";
+import Typical from "react-typical";
 
 export default function Profile() {
-    return (
-        <div className='profile-container'>
-            <div className='profile-parent'>
-                <div className='spacer layer1 profile-details'>
-                    <div className='cols'>
-                        <a href ='https://github.com/Angeredspy' target='_blank' rel='noreferrer'>
-                            <i className ='fa fa-github-alt'></i>
-                        </a>
-                        <a href ='https://destinyboundny.wixsite.com/website'  rel='noreferrer' target='_blank'>
-                            <i className ='fa fa-plus-square'></i>
-                        </a>
-                        <a href ='https://www.goodnewsnetwork.org' target='_blank' rel='noreferrer'>
-                            <i className ='fa fa-globe'></i>
-                        </a>
-                    </div>
-                    <div classname='profile-details-name'>
-                        <span className='primary-text'>
-                            {' '}
-                            Hi! I'm <span className='highlighted-text'>Chris</span>
-                        </span>
-                    </div> 
-                    <div className='profile-detials-role'>
-                        <span className='primary-text'>
-                            {' '}
-                            <h1>
-                                {' '}
-                                <Typical
-                                steps={[
-                                    "Tangible Industry Experience", 
-                                    1000, 
-                                    "Graceful Knowledge Application", 
-                                    1000, 
-                                    "Fluid, Fruitful & Fun Collaboration", 
-                                    1000, 
-                                    "Everlearning Developer", 
-                                    2000
-                                ]}
-                                loop={Infinity}
-                                wrapper="p"
-                                className='typical-class'
-                                />
-                            </h1>
-                        </span>
-                    </div>
-                </div>
+  return (
+    <div className="profile-container">
+      <div className="profile-parent">
+        <div className="spacer layer1 profile-details">
+          <div className="cols">
+            <div className="cols-icon">
+              <a
+                href="https://github.com/Angeredspy"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <i className="fa fa-github-alt"></i>
+              </a>
+              <a
+                href="https://destinyboundny.wixsite.com/website"
+                rel="noreferrer"
+                target="_blank"
+              >
+                <i className="fa fa-plus-square"></i>
+              </a>
+              <a
+                href="https://www.goodnewsnetwork.org"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <i className="fa fa-globe"></i>
+              </a>
             </div>
-
+          </div>
+          <div classname="profile-details-name">
+            <span className="primary-text">
+              {" "}
+              Hi! I'm <span className="highlighted-text">Chris</span>
+            </span>
+          </div>
+          <div className="profile-detials-role">
+            <span className="primary-text">
+              {" "}
+              <h1>
+                {" "}
+                <Typical
+                  steps={[
+                    "Tangible Industry Experience",
+                    1000,
+                    "Graceful Knowledge Application",
+                    1000,
+                    "Fluid, Fruitful & Fun Collaboration",
+                    1000,
+                    "Everlearning Developer",
+                    2000,
+                  ]}
+                  loop={Infinity}
+                  wrapper="p"
+                  className="typical-class"
+                />
+              </h1>
+              <span className="profile-role-tagline">
+                Data Engineer with a knack for front and back end operations.
+              </span>
+            </span>
+          </div>
+          <div className="profile-options">
+            <button className="btn primary-btn">Hire Me</button>
+            <a href="ChristopherGrahamsResume.pdf" download="Chris Graham">
+              <button className="btn highlighted-btn">Get Resume</button>
+            </a>
+          </div>
         </div>
-    )
+        <div className="profile-picture">
+          <div className="profile-picture-background"></div>
+        </div>
+      </div>
+    </div>
+  );
 }
