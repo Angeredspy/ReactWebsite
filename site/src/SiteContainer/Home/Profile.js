a54dd2d684fabd03720f08101a8aecffa9e91a1c
Angeredspy <christo.graham1@gmail.com>
2022-09-13 11:47:54 -0400

    Adds charity link to FA icon

--- site/src/SiteContainer/Home/Profile.js
+++ site/src/SiteContainer/Home/Profile.js
@@ -6,15 +6,21 @@ export default function Profile() {
             <div className='profile-parent'>
                 <div className='profile-details'>
                     <div className='cols'>
-                        <a href ='#'>
+                        <a href ='https://github.com/Angeredspy'>
                             <i className ='fa fa-github-alt'></i>
                         </a>
-                        <a href ='#'>
+                        <a href ='https://destinyboundny.wixsite.com/website'>
                             <i className ='fa fa-plus-square'></i>
                         </a>
-                        <a href ='#'>
+                        <a href ='https://www.goodnewsnetwork.org'>
                             <i className ='fa fa-globe'></i>
                         </a>
+                    </div>
+                    <div classname='profile-details-name'>
+                        <span className='primary-text'>
+                            {' '}
+                            Hi! I'm <span className='highlighted-text'>Chris</span>
+                        </span>
                     </div>
                 </div>
             </div>
