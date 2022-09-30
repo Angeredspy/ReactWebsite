2326a8dc9e9d6a32d70549adb8873cdd13bcb18b
Angeredspy <christo.graham1@gmail.com>
2022-09-30 12:02:25 -0400

    changes heading, abou me component & aboutme css

--- site/src/SiteContainer/AboutMe/AboutMe.js
+++ site/src/SiteContainer/AboutMe/AboutMe.js
@@ -2,11 +2,63 @@ import React, {useEffect} from 'react';
 import ScreenHeading from '../../../src/Utilities/ScreenHeading/ScreenHeading.js';
 import ScrollService from'../../Utilities/ScrollService';
 import Animations from '../../Utilities/Animations';
+import './Aboutme.css'
 
-export default function AboutMe () {
+export default function AboutMe (props) {
+    let fadeInScreenHandler = (screen) => {
+        if(screen.fadeScreen !== props.id) 
+        return 
+        Animations.animations.fadeInScreen(props.id)
+    }
+        const fadeInSubscription = ScrollService.currentScreenFadeIn.subscribe(fadeInScreenHandler)
+        const SCREEN_CONSTANTS = {
+            description: 'I am a seasoned Engineer experienced in building scalable applications & using OOP languages such as Node JS and Python alongside relational DBs. My current goal is to become a Software Engineer for Doctors Without Borders.',
+            highlights: {
+                bullets:[
+                    'Frontend and Backend Web Development (Bootstrap, Typescript)',
+                    'Experienced Database Management & Maintenance',
+                    'T.D.D. paradigms & testing with frameworks like Jest & Jasmine',
+                    'Technical resolutions to internal & external UX issues',
+                    'Critical Task Automations',
+                    'Expert use of 3rd party tooling; see my Technical Landscape!', //tech landscape will replace testimonial
+                ],                                                                                  // and will showcase the technologies i am familiar with and have interacted with extensively 
+                heading: 'Part of what I bring to the table:'
+            }
+        }
+        const renderHighlight = () => {
+            return(
+                SCREEN_CONSTANTS.highlights.bullets.map((value, i) => (
+                    <div className='highlight' key={i}>
+                        <div className='highlight-blob'></div>
+                        <span>{value}</span>
+                    </div>
+                ))
+            )
+        }
+
     return (
-        <div>
-
+        <div className='about-me-container screen-container' id={props.id || ''}>
+                <div className='about-me-parent'>
+                    <ScreenHeading title={'About Me'} subHeading={'Why Choose Me?'} />
+                    <div className='about-me-card'>
+                        <div className='about-me-profile'></div>
+                        <div className='about-me-details'>
+                            <span className='about-me-description'>{SCREEN_CONSTANTS.description}</span>
+                            <div className='about-me-highlights'>
+                                <div className='highlight-heading'>
+                                    <span>{SCREEN_CONSTANTS.highlights.heading}</span>
+                                </div>
+                                {renderHighlight()}
+                            </div>
+                            <div className='about-me-options'>
+                            <button className="btn primary-btn">Hire Me</button>
+                                <a href="ChristopherGrahamsResume.pdf" download="Chris Graham">
+                                     <button className="btn highlighted-btn">Get Resume</button>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
         </div>
     )
 }
--- site/src/Utilities/ScreenHeading/ScreenHeading.js
+++ site/src/Utilities/ScreenHeading/ScreenHeading.js
@@ -1,28 +1,26 @@
 import React from 'react';
 import './ScreenHeading.css';
 
-
 export default function ScreenHeading(props) {
     return (
         <div className='heading-container'> 
             <div className='screen-heading'>
                 <span>{props.title}</span>
             </div>
-            {
-                (props.subheading) ? (
+
+                {(props.subHeading) ? (
                     <div className='screen-sub-heading'> 
-                        <span> {props.subheading} </span>
+                        <span>{props.subHeading}</span>
                     </div>
-                ) : <div> </div>
-            }
+                ) : ( 
+                <div></div>
+            )}
 
             <div className='heading-seperator'>
-                <div className='seperator-line'>
                     <div className='seperator-blob'>
-                        <div> </div>
+                        <div></div>
                     </div>
-                </div>
             </div>
         </div>
-    )
+    );
 }
