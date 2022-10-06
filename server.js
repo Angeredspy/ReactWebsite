09102cddbdab8fd23aefa09c872b311ebb144094
Angeredspy <christo.graham1@gmail.com>
2022-10-06 13:07:40 -0400

    corrects filepaths

--- server.js
+++ server.js
@@ -14,9 +14,9 @@ app.use(cors())
 app.use('/', contactRoute);
 
 if(process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'))
+    app.use(express.static('site/build'))
     app.get('*',(req,res)=>
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+        res.sendFile(path.resolve(__dirname, 'site', 'build', 'index.html'))
     );
 }
 
