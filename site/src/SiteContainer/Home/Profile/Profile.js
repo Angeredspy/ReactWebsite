3fc0b766d7b4a1053090803f2c0cd7962ea464df
Chris-Graham-MSF <Christopher.graham@newyork.msf.org>
2023-04-03 19:59:46 -0400

    creates placeholder for food pantry information

--- site/src/SiteContainer/Home/Profile/Profile.js
+++ site/src/SiteContainer/Home/Profile/Profile.js
@@ -3,6 +3,7 @@ import Typical from "react-typical";
 import './Profile.css';
 import ScrollService from "../../../Utilities/ScrollService";
 import smoothTunes from '../../../Assets/Home/Shopping.mp3'; //Base is ...beneaththemask.mp3
+import Modal from "react-modal";
 
 let music = new Audio(smoothTunes)
 
@@ -14,8 +15,34 @@ let pause = () => {
   music.pause();
 }
 
+// Binds modal to app element
+Modal.setAppElement('#root');
+
 export default function Profile() {
   const [count, setCount] = useState(1);
+  const [activeIcon, setActiveIcon] = useState(null);
+
+  const closeModal = () => {
+    setActiveIcon(null);
+  };
+
+  const openModal = (icon) => {
+    setActiveIcon(icon);
+  };
+    // Define your modal styles
+    const customStyles = {
+      content: {
+        top: '50%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        marginRight: '-50%',
+        transform: 'translate(-50%, -50%)',
+        width: '50%',
+        padding: '2rem',
+      },
+    };
+
   return (
     <div className="profile-container padding-div">
       <div className="profile-parent">
@@ -58,6 +85,23 @@ export default function Profile() {
               >
                 <i className="fa fa-globe"></i>
               </a>
+              <a
+            onClick={() => openModal("globe")}
+            href="#"
+            title="Open modal for globe icon"
+          >
+            <i className="fa fa-globe"></i>
+          </a>
+          <Modal
+        isOpen={activeIcon === "globe"}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Globe Modal"
+      >
+        <h2>Globe Icon Modal</h2>
+        <p>Food Pantry information will go here.</p>
+        <button onClick={closeModal}>Close</button>
+      </Modal>
             </div>
           </div>
           <div className="profile-details-name">
