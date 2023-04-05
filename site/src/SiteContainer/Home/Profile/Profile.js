9415d1b99f784734439e7beff99701f6e32dc8bb
Chris-Graham-MSF <Christopher.graham@newyork.msf.org>
2023-04-04 22:00:56 -0400

    adds gradual fade in for pantry modal

--- site/src/SiteContainer/Home/Profile/Profile.js
+++ site/src/SiteContainer/Home/Profile/Profile.js
@@ -29,19 +29,19 @@ export default function Profile() {
   const openModal = (icon) => {
     setActiveIcon(icon);
   };
-    // Define your modal styles
-    const customStyles = {
-      content: {
-        top: '50%',
-        left: '50%',
-        right: 'auto',
-        bottom: 'auto',
-        marginRight: '-50%',
-        transform: 'translate(-50%, -50%)',
-        width: '50%',
-        padding: '2rem',
-      },
-    };
+    
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+    width: '50%',
+    padding: '2rem',
+  },
+};
 
   return (
     <div className="profile-container padding-div">
@@ -90,15 +90,16 @@ export default function Profile() {
             href="#"
             title="Open modal for globe icon"
           >
-            <i className="fa fa-globe"></i>
+            <i className="fa fa-spoon"></i>
           </a>
           <Modal
         isOpen={activeIcon === "globe"}
         onRequestClose={closeModal}
         style={customStyles}
         contentLabel="Globe Modal"
+        overlayClassName="modal-overlay"
       >
-        <h2>Globe Icon Modal</h2>
+        <h2>Food Pantry Modal</h2>
         <p>Food Pantry information will go here.</p>
         <button onClick={closeModal}>Close</button>
       </Modal>
